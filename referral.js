eef7ad3f2a1b32f0b8e2e4dfbdf2ef9bf433d485
Srikarboske <boskesrikar@gmail.com>
2023-10-27 15:52:22 +0530

    Romi Finance
    Romi Finance

---
+++ referral.js
@@ -0,0 +1,16 @@
+function gettrade()
+{
+    title="Enter Referral Code"
+    text="Please input a referral code to benefit from fee discounts."
+    document.getElementById("title").innerHTML=title;
+    document.getElementById("text").innerText=text;
+}
+function getaffiliates()
+{
+    title="Generate Referral Code"
+    text="Looks like you don't have a referral code to share.Create one now and start earning rebates!"
+    document.getElementById("title").innerHTML=title
+    document.getElementById("text").innerText=text;
+
+}
+
